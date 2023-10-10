a48049e7e19f7c56d484f626abcd394252773657
FabrycioMedeiros <fabrycioerico.engmecanico@gmail.com>
2023-10-10 17:00:28 -0300

    Criação da pasta models e arquivo Livro.js

---
+++ src/models/Livro.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const livroSchema = new mongoose.Schema({
+    id: { type: mongoose.Schema.Types.ObjectId },
+    titulo: { type: String, require: true },
+    editora: { type: String },
+    preco: { type: Number },
+    paginas: { type: Number }
+},{ versionKey: false });
+// versionKey se refere ao versionamento do mongo DB
+
+const livro = mongoose.model("livros", livroSchema);
+
+export default livro;
+
+// ,odelo é um objeto que representa uma coleção na base de dados
