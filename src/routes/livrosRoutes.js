db6a31f6ec8c09385f9a7630a030d350cc37df44
FabrycioMedeiros <fabrycioerico.engmecanico@gmail.com>
2023-10-11 10:02:37 -0300

    Correção de código

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,3 +4,5 @@ import LivroController from "../controllers/livroController.js";
 const routes = express.Router();
 //criação de rota .get
 routes.get("/livros",LivroController.listarLivros);
+
+export default routes;
