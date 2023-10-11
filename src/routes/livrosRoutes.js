3c4d6dbdd3a8a0a14ad85852d82274b226552c9e
FabrycioMedeiros <fabrycioerico.engmecanico@gmail.com>
2023-10-11 16:18:19 -0300

    Adição de informação comentada

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -3,7 +3,9 @@ import LivroController from "../controllers/livroController.js";
 
 const routes = express.Router();
 //criação de rota .get
+//Obs IMPORTANTE - as rotas devem seguir uma ordem de maior complexidade para menor
 routes.get("/livros", LivroController.listarLivros);
+routes.get("/livros/busca", LivroController.listarLivrosPorEditora);
 routes.get("/livros/:id", LivroController.listarLivroPorId);
 routes.post("/livros", LivroController.cadastrarLivro);
 routes.put("/livros/:id", LivroController.atualizarLivroPorId);
