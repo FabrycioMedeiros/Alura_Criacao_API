c249ac4329dfa28a8c0d38ceeaf6a55b30e12d85
FabrycioMedeiros <fabrycioerico.engmecanico@gmail.com>
2023-10-11 15:43:21 -0300

    Correção no import autorSchema { }

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
+import { autorSchema } from "./Autor.js"
 
 const livroSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
     titulo: { type: String, require: true },
     editora: { type: String },
     preco: { type: Number },
-    paginas: { type: Number }
+    paginas: { type: Number },
+    autor: autorSchema
 },{ versionKey: false });
 // versionKey se refere ao versionamento do mongo DB
-
+ 
 const livro = mongoose.model("livros", livroSchema);
 
 export default livro;
