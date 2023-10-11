179fd2c4de8950a73bbf4ce00da65a0c7890cb3f
FabrycioMedeiros <fabrycioerico.engmecanico@gmail.com>
2023-10-11 13:35:44 -0300

    Criação dos controllers do CRUD

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -4,9 +4,23 @@ import livro from "../models/Livro.js";
 class LivroController {
 
 static async listarLivros (req, res) {
-    // controller chama o model Livro através do método livro.find({})
-    const listaLivros = await livro.find({});
+    try{
+// controller chama o model Livro através do método livro.find({})
+     const listaLivros = await livro.find({});
      res.status(200).json(listaLivros);
+    } catch (erro) {
+    res.status(500).json({ message: `${erro.message} - falha na requisição` });
+    } 
+};
+
+static async listarLivroPorId (req, res) {
+    try{
+     const id = req.params.id;
+     const livroEncontrado = await livro.findById(id);
+     res.status(200).json(livroEncontrado);
+    } catch (erro) {
+    res.status(500).json({ message: `${erro.message} - falha na requisição do livro` });
+    } 
 };
 
 static async cadastrarLivro (req, res) {
@@ -14,8 +28,28 @@ static async cadastrarLivro (req, res) {
     const novoLivro = await livro.create(req.body);
     res.status(201).json({ message: "Criado com sucesso!", livro: novoLivro });
    } catch (erro) {
-     res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro`});
+     res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro` });
    } 
+};
+
+static async atualizarLivroPorId (req, res) {
+    try{
+     const id = req.params.id;
+     await livro.findByIdAndUpdate(id, req.body);
+     res.status(200).json({ message: "livro atualizado" });
+    } catch (erro) {
+    res.status(500).json({ message: `${erro.message} - falha na atualização` });
+    } 
+};
+
+static async excluirLivro (req, res) {
+    try{
+     const id = req.params.id;
+     await livro.findByIdAndDelete(id);
+     res.status(200).json({ message: "livro excluido com sucesso" });
+    } catch (erro) {
+    res.status(500).json({ message: `${erro.message} - falha na exclusão` });
+    } 
 };
 
 };
