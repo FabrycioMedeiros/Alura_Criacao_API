db38273ee92fcf41b8b4ffad303c3721e7e6e3f1
FabrycioMedeiros <fabrycioerico.engmecanico@gmail.com>
2023-10-11 13:34:54 -0300

    Criação de rotas .get, .post, .put e .delete

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -3,6 +3,11 @@ import LivroController from "../controllers/livroController.js";
 
 const routes = express.Router();
 //criação de rota .get
-routes.get("/livros",LivroController.listarLivros);
+routes.get("/livros", LivroController.listarLivros);
+routes.get("/livros/:id", LivroController.listarLivroPorId);
+routes.post("/livros", LivroController.cadastrarLivro);
+routes.put("/livros/:id", LivroController.atualizarLivroPorId);
+routes.delete("/livros/:id", LivroController.excluirLivro);
+
 
 export default routes;
